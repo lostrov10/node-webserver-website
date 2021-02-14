6e75176e0f44a18fdd40b7eed38cdcef6467b3cf
lioro <lioro@netapp.com>
2021-02-14 09:20:02 +0200

    adding humidity to forcast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude , longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location.', undefined)
         } else {
-            callback(undefined, 'The weather is ' + body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out in ' + body.location.region + ', It feels like ' + body.current.feelslike + ' degress out.')
+            callback(undefined, 'The weather is ' + body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out in ' + body.location.region + ', It feels like ' + body.current.feelslike + ' degress out and the humidity is: ' + body.current.humidity)
         }
     })
 }
